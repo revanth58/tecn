a1800e5f139db8856b71414f4adbee8ddf4b874b
revanth58 <revanthk7777@gmail.com>
2026-09-19 20:28:08 +0530

    vsdvs

--- codes/g3.c
+++ codes/g3.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
+
+// from a single c program fork and spawn a child process
+// 1st child to run a compile command
+// 2nd child to run the executable
+
+
 
 int main(){
-    int k = fork();
+	int z = fork();
 
-    int *x = (int *)malloc(sizeof(int)*10);
-    *x = 12;
+	if(z == 0)execlp("gcc","gcc", "g2.c","-o","b.out",NULL);
+	wait(NULL);
+	printf("compilation done...\nrunning b.out...\n");
 
-    if(k==0){
-        *x=10;
-
-        printf("i am child, ptr = %p, value is %d")
-    }
+	z = fork();
+	if( z ==0 )execlp("./b.out","./b.out",NULL);
 }
+
+// to run , you need a test c file named g2.c in your folder
+// run gcc g3.c, and ./a.out
